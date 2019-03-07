3f05b81093c8824af3ada9041d2d4eb330a622aa
evan <evanisme@live.com>
2019-03-06 20:40:00 -0800

    use evan variable

--- windows_shims/conda.bat
+++ windows_shims/conda.bat
@@ -1,1 +1,1 @@
-@bash.exe -c '. "/home/evan/.miniconda3/etc/profile.d/conda.sh"; CONDA_CHANGEPS1=false conda activate base; conda %*'
+@bash.exe -c '. "/home/$USER/.miniconda3/etc/profile.d/conda.sh"; CONDA_CHANGEPS1=false conda activate base; conda %*'
